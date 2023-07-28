b0a5e8b39bd0b232b9358c611e76017bff4331c8
malak.mukhtarli <malak.mukhtarli@rabita.az>
2023-07-28 18:51:44 +0400

    initial

---
+++ script.js
@@ -0,0 +1,8 @@
+$.fn.select2.defaults.set('amdLanguageBase', 'select2/i18n/');
+$(document).ready(function() {
+    $('.js-example-basic-multiple').select2();
+});
+
+// $(".js-example-basic-multiple-limit").select2({
+//     maximumSelectionLength: 10
+//   });
